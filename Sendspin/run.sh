a39a8d0b10dba4e9cb84a3c5e960ebe36e23a828
Switch <25269380+Switch123456789@users.noreply.github.com>
2026-03-30 01:17:38 +0200

    Improved volume-handling

--- Sendspin/run.sh
+++ Sendspin/run.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/with-contenv bashio
 CFG_PATH="/root/.config/sendspin/settings-daemon.json"
-SENDSPIN_CMD="sendspin daemon --audio-device 0 --disable-mpris"
+SENDSPIN_CMD="sendspin daemon --disable-mpris --hardware-volume false"
 function check_config {
-    declare -g VALUE="$(bashio::config "$2")"
-    if [ "${VALUE}" != null ]; then if [ "$1" = str ]
-        then declare -g SENDSPIN_CMD+=" ${3} \"${VALUE}\""
-        else declare -g SENDSPIN_CMD+=" ${3} ${VALUE}"
-    fi; fi
+    declare -g VALUE="$(bashio::config "$1")"
+    if [ "${VALUE}" != null ]; then
+        if   [ "$1" = boost_volume ]; then
+            if [ "${VALUE}" = true ]; then bashio::log.info "SETTING VOLUME TO 100% ..."; amixer -q set Master 100%; fi
+        elif [ "$1" = static_delay_ms ]; then
+            declare -g SENDSPIN_CMD+=" ${2} ${VALUE}"
+        else
+            declare -g SENDSPIN_CMD+=" ${2} \"${VALUE}\""    
+        fi
+    fi
 }
 
+check_config boost_volume    false
+check_config static_delay_ms "--static-delay-ms"
+check_config name            "--name"
+check_config id              "--id"
+check_config url             "--url"
+check_config log_level       "--log-level"
 
-bashio::log.info "Checking config-file:"
-bashio::log.info "$CFG_PATH"
-check_config int static_delay_ms "--static-delay-ms"
-check_config str name            "--name"
-check_config str id              "--id"
-check_config str url             "--url"
-check_config str log_level       "--log-level"
+bashio::log.info "CHECKING CONFIGURATION FILE ..."
 jq -r 'to_entries[] |  "\(.key): \(.value)"' "${CFG_PATH}"
 
-
-bashio::log.info "Starting with command:"
-bashio::log.info "${SENDSPIN_CMD}"
+bashio::log.info "STARTING SENDSPIN ..."
 eval "${SENDSPIN_CMD}"
